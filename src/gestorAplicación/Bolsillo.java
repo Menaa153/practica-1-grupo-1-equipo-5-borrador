de2efc7393a892eb1403a14426236722ce95e2bf
ihernandezss <118498504+ihernandezss@users.noreply.github.com>
2023-04-21 15:51:14 -0500

    Update Bolsillo.java

---
+++ src/gestorAplicación/Bolsillo.java
@@ -0,0 +1,14 @@
+package gestorAplicación.classpropuestas;
+
+public class Bolsillo extends Cuenta {
+	private Categoria categoria;
+	
+	//constructor)
+	public Bolsillo (Usuario usuario, Categoria Categoria) {
+		super(usuario);
+		this.categoria=categoria;
+		
+	}
+	
+
+}
