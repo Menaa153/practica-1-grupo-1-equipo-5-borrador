0599f82ba97c9a11a175e97432a26a8982669f4e
ihernandezss <118498504+ihernandezss@users.noreply.github.com>
2023-04-21 15:48:40 -0500

    Create Ingreso.java

---
+++ src/gestorAplicación/Ingreso.java
@@ -0,0 +1,39 @@
+package gestorAplicación.classpropuestas;
+import java.ArrayList;
+
+public class Ingreso extends Transaccion{
+	private Cuenta cuentaOrigen;
+	private Cuenta cuentaDestino;
+	private Categoria categoria;
+
+	public Ingreso(int monto, LocalDate fechaCreacion, Cuenta cuentaOrigen, Cuenta cuentaDestino) {
+		this (monto, fechaCreacion, cuentaOrigen, cuentaDestino, null);
+	}
+	public Ingreso (int monto, LocalDate fechaCreacion, Cuenta cuentaOrigen, Cuenta cuentaDestino, Categoria categoria) {
+		super(monto, fechaCreacion);
+		this.cuentaOrigen=cuentaOrigen;
+		this.cuentaDestino=cuentaDestino;
+		this.categoria=categoria;
+		
+	
+	}
+	//getters y setters
+	public void setCuentaOrigen(Cuenta cuentaOrigen) {
+		this.cuentaOrigen=cuentaOrigen;
+	}
+	public void setCuentaDestino(Cuenta cuentaDestino) {
+		this.cuentaDestino=cuentaDestino;
+	}
+	public void setCategoria(Categoria categoria) {
+		this.categoria=categoria;
+	}
+	public Cuenta getCuentaOrigen() {
+		return cuentaOrigen;
+	}
+	public Cuenta getCuentaDestino() {
+		return cuentaDestino;
+	}
+	public Categoria getCategoria() {
+		return categoria;
+	}
+}
