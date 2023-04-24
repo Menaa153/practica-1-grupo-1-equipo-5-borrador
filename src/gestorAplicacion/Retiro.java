f8869735dd45accf05a7cd3f7054b3c92db574b7
amgg16 <124928492+amgg16@users.noreply.github.com>
2023-04-24 15:34:19 -0500

    :+1:

---
+++ src/gestorAplicacion/Retiro.java
@@ -0,0 +1,40 @@
+package gestorAplicacion;
+
+import java.time.LocalDate;
+
+public class Retiro extends Transaccion{
+	private Cuenta cuentaOrigen;
+	private Cuenta cuentaDestino;
+	private Categoria categoria;
+
+	public Retiro(int monto, LocalDate fechaCreacion, Cuenta cuentaOrigen, Cuenta cuentaDestino) {
+		this (monto, fechaCreacion, cuentaOrigen, cuentaDestino, null);
+	}
+	public Retiro (int monto, LocalDate fechaCreacion, Cuenta cuentaOrigen, Cuenta cuentaDestino, Categoria categoria) {
+		super(monto, fechaCreacion);
+		this.cuentaOrigen=cuentaOrigen;
+		this.cuentaDestino=cuentaDestino;
+		this.categoria=categoria;
+	}
+	//getters y setters
+	public void setCuentaOrigen(Cuenta cuentaOrigen) {
+		this.cuentaOrigen=cuentaOrigen;
+	}
+	public void setCuentaDestino(Cuenta cuentaDestino) {
+		this.cuentaDestino=cuentaDestino;
+	}
+	public void setCategoria(Categoria categoria) {
+		this.categoria=categoria;
+	}
+	public Cuenta getCuentaOrigen() {
+		return cuentaOrigen;
+	}
+	public Cuenta getCuentaDestino() {
+		return cuentaDestino;
+	}
+	public Categoria getCategoria() {
+		return categoria;
+	}
+}
+
+
