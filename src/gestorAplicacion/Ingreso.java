22053137320ca2aab3f4f5d8acac0be2975f85e6
ihernandezss <118498504+ihernandezss@users.noreply.github.com>
2023-04-24 15:55:31 -0500

    Update Ingreso.java

--- src/gestorAplicacion/Ingreso.java
+++ src/gestorAplicacion/Ingreso.java
@@ -8,7 +8,7 @@ public class Ingreso extends Transaccion{
 	private Categoria categoria;
 
 	public Ingreso(int monto, LocalDate fechaCreacion, Cuenta cuentaOrigen, Cuenta cuentaDestino) {
-		this (monto, fechaCreacion, cuentaOrigen, cuentaDestino, Categoria.nulo);
+		this (monto, fechaCreacion, cuentaOrigen, cuentaDestino, Categoria.Nulo);
 	}
 	public Ingreso (int monto, LocalDate fechaCreacion, Cuenta cuentaOrigen, Cuenta cuentaDestino, Categoria categoria) {
 		super(monto, fechaCreacion);
